94d2866386ee8fa3e57c6eb7dcccb442c7bff9b4
norma <gabriela.nav1103@gmail.com>
2025-02-12 08:17:55 -0400

    useState

--- src/01-hook-app/02-useEfect/FormLogin.jsx
+++ src/01-hook-app/02-useEfect/FormLogin.jsx
@@ -1,15 +1,21 @@
 import { MessageApp } from "./MessageApp";
 
+const initialState = {
+  username: "norma",
+  password: 345,
+};
+
 export const FormLogin = () => {
+  const [form, setForm] = useState(initialState);
   return (
     <>
       <h1>Login</h1>
       <hr />
 
-      <input type="text" name="username" value="" />
+      <input type="text" name="username" value= {form.username} />
       <br />
       <br />
-      <input type="password" name="password" value="" />
+      <input type="password" name="password" value= {form.password} />
 
       <br />
       <button>Acceder</button>
