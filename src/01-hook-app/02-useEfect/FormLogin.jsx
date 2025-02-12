6d56b036dbe7d1df56ff9c0d4c593876c3b3027d
rodrigo-medrano <rodrigo.mespa.123@gmail.com>
2025-02-12 08:13:20 -0400

    Tercer commit

--- src/01-hook-app/02-useEfect/FormLogin.jsx
+++ src/01-hook-app/02-useEfect/FormLogin.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { MessageApp } from "./MessageApp";
 
+const initialState = {
+  username: "",
+  password: ""
+};
+
 export const FormLogin = () => {
+  const [{username,password}, setState] = useState(initialState);
   return (
     <>
       <h1>Login</h1>
       <hr />
 
-      <input type="text" name="username" value="" />
+      <input type="text" name="username" value={username} />
       <br />
       <br />
-      <input type="password" name="password" value="" />
+      <input type="password" name="password" value={password} />
 
       <br />
       <button>Acceder</button>
