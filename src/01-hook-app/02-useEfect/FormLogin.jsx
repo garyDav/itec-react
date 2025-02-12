33b664152b365c0916cfddd140b602bb115f21f3
unknown <msderam@gmail.com>
2025-02-12 08:17:11 -0400

    ejemplo usesatate rmariane

--- src/01-hook-app/02-useEfect/FormLogin.jsx
+++ src/01-hook-app/02-useEfect/FormLogin.jsx
@@ -1,15 +1,19 @@
 import { MessageApp } from "./MessageApp";
 
+const initialSatate=()=>{
+    const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+}
 export const FormLogin = () => {
   return (
     <>
       <h1>Login</h1>
       <hr />
 
-      <input type="text" name="username" value="" />
+      <input type="text" name="username" value={username} />
       <br />
       <br />
-      <input type="password" name="password" value="" />
+      <input type="password" name="password" value={password}/>
 
       <br />
       <button>Acceder</button>
