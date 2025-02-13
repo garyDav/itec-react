9e785eeb407c067d743135b41acf3b0b12529f15
norma <gabriela.nav1103@gmail.com>
2025-02-13 08:05:30 -0400

    merge

--- src/01-hook-app/02-useEfect/FormLogin.jsx
+++ src/01-hook-app/02-useEfect/FormLogin.jsx
@@ -1,21 +1,34 @@
+import { useState } from "react";
 import { MessageApp } from "./MessageApp";
 
 const initialState = {
-  username: "norma",
-  password: 345,
+  username: "",
+  password: "",
 };
 
 export const FormLogin = () => {
   const [form, setForm] = useState(initialState);
+  const [state, setState] = useState(initialState);
+  const { username, password } = state;
+
   return (
     <>
       <h1>Login</h1>
       <hr />
 
       <input type="text" name="username" value= {form.username} />
+      <input
+        type="text"
+        name="username"
+        value={username}
+        onChange={(event) => {
+          setState({ ...state, username: event.target.value });
+        }}
+      />
       <br />
       <br />
       <input type="password" name="password" value= {form.password} />
+      <input type="password" name="password" value={password} />
 
       <br />
       <button>Acceder</button>
