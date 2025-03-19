db1f6e036221e7ae698bf34d0b99fae30e56d39e
garyDav <gary.2810.dav@gmail.com>
2025-03-19 08:32:33 -0400

    hook: useReducer-intro

--- src/01-hook-app/05-useReducer/todoReducer.js
+++ src/01-hook-app/05-useReducer/todoReducer.js
@@ -1,22 +1,7 @@
 export const todoReducer = (initialState = [], action) => {
   switch (action.type) {
-    case "[TODO] add Todo":
+    case "[TODO] add todo":
       return [...initialState, action.payload];
-
-    case "[TODO] Remove Todo":
-      return initialState.filter((todo) => todo.id !== action.payload);
-
-    case "[TODO] Toggle Todo":
-      return initialState.map((todo) => {
-        if (todo.id === action.payload) {
-          return {
-            ...todo,
-            done: !todo.done,
-          };
-        }
-
-        return todo;
-      });
 
     default:
       return initialState;
