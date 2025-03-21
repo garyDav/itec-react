9c85e757f4f2b0af8329d80eac38a02002df7ac5
garyDav <gary.2810.dav@gmail.com>
2025-03-21 15:09:29 -0400

    useReducer: todo reducer

--- src/01-hook-app/05-useReducer/TodoApp.jsx
+++ src/01-hook-app/05-useReducer/TodoApp.jsx
@@ -1,9 +1,4 @@
-import { UseForm } from "../hooks/useForm";
 import { useTodos } from "../hooks/useTodos";
-
-const initialState = {
-  todo: "",
-};
 
 export const TodoApp = () => {
   const {
@@ -14,22 +9,6 @@ export const TodoApp = () => {
     handleDeleteTodo,
     handleToggleTodo,
   } = useTodos();
-
-  const { formValues, onInputChange, onReset } = UseForm(initialState);
-
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-
-    const newTodo = {
-      id: new Date().getTime(),
-      done: false,
-      todo: formValues.todo,
-    };
-
-    console.log(newTodo);
-    handleNewTodo(newTodo);
-    onReset();
-  };
 
   return (
     <>
